00689c7392f5300a467a1248f488f3feb7ab7ed3
Josh Chaiken <me@joshchaiken.com>
2021-02-06 18:07:38 -0500

    small hero updates, https enabled if vars are correct

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const ENV = process.env.ENV || 'development';
+const USE_HTTPS = process.env.USE_HTTPS || false;
 
 app.use('/', express.static('dist'))
 
+if(ENV === 'production' && USE_HTTPS) {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 app.listen(PORT, () => console.log(`Your app is listening on port ${PORT}!`));
+
