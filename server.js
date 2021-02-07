23b593fa923772251850ca87a3dc2ffbb5a61c35
Josh Chaiken <me@joshchaiken.com>
2021-02-07 00:51:32 -0500

    fixing the heroku+https

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const ENV = process.env.ENV || 'development';
-const USE_HTTPS = process.env.USE_HTTPS || false;
 
 app.use('/', express.static('dist'))
 
-if(ENV === 'production' && USE_HTTPS) {
+if (ENV === 'production') {
   app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https')
       res.redirect(`https://${req.header('host')}${req.url}`)
