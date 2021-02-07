14aedc09940b49cf77ae312dbce655f0e77b4bfd
Josh Chaiken <me@joshchaiken.com>
2021-02-07 01:06:28 -0500

    silly packages, supposed to make things easier, until they dont. oh well this should fix it up

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const sslRedirect = require('heroku-ssl-redirect');
+const sslRedirect = require('heroku-ssl-redirect').default;
 const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 const ENV = process.env.ENV || 'development';
 
-app.use('/', express.static('dist'))
+ENV === "production" && app.use('/', express.static('dist'))
 
-if (ENV === 'production') {
-  app.use(sslRedirect());
-}
+app.use(sslRedirect());
 
 app.listen(PORT, () => console.log(`Your app is listening on port ${PORT} running in ${ENV} mode.`));
 
