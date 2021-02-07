3c301f0065d33b2509f5ddc3026ee094bf196fa8
Josh Chaiken <me@joshchaiken.com>
2021-02-07 00:54:18 -0500

    one more debug message for the deployment

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use('/', express.static('dist'))
 if (ENV === 'production') {
   app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https')
+      console.log("detected http, redirecting to https")
       res.redirect(`https://${req.header('host')}${req.url}`)
     else
       next()
