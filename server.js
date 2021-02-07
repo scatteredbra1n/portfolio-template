77e199b7ef47b004206deca20ca9b42d7a0a16d8
Josh Chaiken <me@joshchaiken.com>
2021-02-07 10:22:36 -0500

    Order matters

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const ENV = process.env.ENV || 'development';
 
-ENV === "production" && app.use('/', express.static('dist'))
+ENV === "production" && app.use(sslRedirect());
 
-app.use(sslRedirect());
+app.use('/', express.static('dist'));
 
 app.listen(PORT, () => console.log(`Your app is listening on port ${PORT} running in ${ENV} mode.`));
 
