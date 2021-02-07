36b613de232356c727c17710989ec691eff8b855
Josh Chaiken <me@joshchaiken.com>
2021-02-07 00:56:39 -0500

    that's what i get for copy-pastaing from stack overflow i guess!

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ app.use('/', express.static('dist'))
 
 if (ENV === 'production') {
   app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https')
-      console.log("detected http, redirecting to https")
-      res.redirect(`https://${req.header('host')}${req.url}`)
-    else
-      next()
+    if (req.header('x-forwarded-proto') !== 'https') {
+		console.log("detected http, redirecting to https")
+		res.redirect(`https://${req.header('host')}${req.url}`)	
+    } else {
+    	next()
+    }
   })
 }
 
