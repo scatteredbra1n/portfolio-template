c6f87c0d76e4d209d8a9ebe73db7072f7e256276
Josh Chaiken <me@joshchaiken.com>
2021-02-07 00:59:07 -0500

    when you cant fix it yourself, use a package

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import sslRedirect from 'heroku-ssl-redirect';
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -6,14 +7,7 @@ const ENV = process.env.ENV || 'development';
 app.use('/', express.static('dist'))
 
 if (ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https') {
-		console.log("detected http, redirecting to https")
-		res.redirect(`https://${req.header('host')}${req.url}`)	
-    } else {
-    	next()
-    }
-  })
+  app.use(sslRedirect());
 }
 
 app.listen(PORT, () => console.log(`Your app is listening on port ${PORT} running in ${ENV} mode.`));
